94078cafeae42f7850876fcd86954da2056dc527
kyungmin <kmin.life2@gmail.com>
2026-01-02 10:25:32 +0900

    styled-components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,26 @@
+import styled from "styled-components";
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  background-color: black;
+
+  ul {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+  }
+
+  li {
+    list-style: none;
+    font-weight: 400;
+    font-size: 13px;
+  }
+`;
+
 export default function Header() {
   return (
     <header className="header-container">
